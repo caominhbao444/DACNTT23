14d34a5d908e596d65c7b18b6f1acba597855485
Huynh Chanh <chanhhuynhst7@gmail.com>
2023-04-19 14:46:46 +0700

    update createMessage

--- BA/back-end/controllers/messageController.js
+++ BA/back-end/controllers/messageController.js
@@ -1,28 +1,22 @@
 const asyncHandler = require("express-async-handler");
 const Message = require("../models/messageModel");
 const Conversation = require("../models/conversationModel");
-const Account = require("../models/accountModel")
+const Account = require("../models/accountModel");
 
 const createMessage = asyncHandler(async (req, res) => {
   try {
-    const account = await Account.findOne(req.account);
-    if(account){
-        const message = new Message({
-          conversationId: req.params.id,
-          senderId: account._id,
-          fullname : account.fullname,
-          text: req.body.text,
-        });
-        const savedMessage = await message.save();
-        res.status(200).json(savedMessage);
-    }else{
-      res.status(404).json("Account Not Found")
-    }
+    const message = new Message({
+      conversationId: req.params.id,
+      senderId: req.account.id,
+      fullname: req.account.fullname,
+      text: req.body.text,
+    });
+    const savedMessage = await message.save();
+    res.status(200).json(savedMessage);
   } catch (err) {
     res.status(500).json(err);
   }
 });
-
 
 //params = receiverId => conversation (receiverId : req.params.id && senderId : account._id)
 const getMessage = asyncHandler(async (req, res) => {
@@ -40,17 +34,15 @@ const getMessage = asyncHandler(async (req, res) => {
   // } catch (err) {
   //   res.status(500).json(err);
   // }
-  const message = await Message.find({conversationId :req.params.id});
+  const message = await Message.find({ conversationId: req.params.id });
   res.status(200).json(message);
 });
 
 const testm = asyncHandler(async (req, res) => {
-
   const messages = await Conversation.find({ receiverId: req.params.id });
-  const conversationIds = messages.map(message => message._id);
+  const conversationIds = messages.map((message) => message._id);
 
   res.status(200).json(conversationIds);
-
 });
 
 module.exports = { createMessage, getMessage, testm };
